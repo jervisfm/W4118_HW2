f43d7b37b9fda490a22eb18968b502d88aa3eaf2
Shane Chin <sac2171@columbia.edu>
2012-09-29 20:29:40 +0000

    Added print_prinfo

--- test-program/test.c
+++ test-program/test.c
@@ -19,11 +19,23 @@ int main()
 	int i;
 	for (i = 0; i < nr; i++) {
 		struct prinfo p = buf[i];
-		printf("%s,%d,%ld,%d,%d,%d,%ld\n", p.comm, p.pid, p.state,
-			p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid);
-		//printf("%s\n", buf[i].comm);
+				//printf("%s\n", buf[i].comm);
 	}
 
 	printf("\nTotal entries: %d\n", rc);
 	return 0;
+}
+
+void print_tree(struct prinfo *tree, int size){
+}
+
+void print_prinfo(int count){
+	char returned[count+1];
+	int i = 0;
+	for(i=0; i<count; i++)
+		returned[i] = '\t'; 
+	returned[count] = '\0';
+	printf("%s %s,%d,%ld,%d,%d,%d,%ld\n", &returned, p.comm, p.pid, p.state,
+			p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid);
+
 }
